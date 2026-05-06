1f1ee4a7b4c0ff4c8531934af088057410b2b3a1
S4-POSTE273 <S4-POSTE273@Sinfo4.pigierci.ci>
2026-05-06 10:23:07 +0000

    Mise à jour des fichiers Java existants

--- src/bookstoread/BookShelfSpec.java
+++ src/bookstoread/BookShelfSpec.java
@@ -1,16 +1,27 @@
 package bookstoread;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
-
 import org.junit.jupiter.api.Test;
 
 public class BookShelfSpec {
+
     @Test
-    public void shelfEmptyWhenNoBookAdded() throws Exception {
+    public void shelfEmptyWhenNoBookAdded() {
         BookShelf shelf = new BookShelf();
         List<String> books = shelf.books();
-        assertTrue(books.isEmpty(), () -> "BookShelf should be empty.");
+        assertTrue(books.isEmpty(), () -> "L'étagère devrait être vide.");
+    }
+
+    @Test
+    void bookshelfContainsTwoBooksWhenTwoBooksAdded() {
+        BookShelf shelf = new BookShelf();
+
+        shelf.add("Effective Java", "Code Complete");
+
+        List<String> books = shelf.books();
+        assertEquals(2, books.size(), () -> "L'étagère devrait contenir deux livres.");
     }
 }
